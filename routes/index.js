a3db1a0078bf053675538b2d33a699b27cf6fdc7
Mariana Avalos <mariana.avalos.arce@gmail.com>
2019-09-16 21:03:07 -0500

    login with txt file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 var tools = require("./fs-tools.js");
 
 /* 1. GET home page. */
@@ -15,14 +16,14 @@ router.get('/users', function(req, res, next) {
 
 // goes here if we type localhost:8000/users/detail
 router.get('/users/detail', function(req, res, next) {
-  res.send('detail');
+  res.send('details');
 });
 
 router.post('/login', function(req, res, next){
 
   //check validity with validator pckg function
   //req.check('address', 'invalid email address').isEmail(); // email has to match name
-  var valid = tools.check({ file: "accounts.txt", account: req.body.address });
+  //var valid = tools.check({ file: "accounts.txt", account: req.body.address });
   req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
 
   var errors = req.validationErrors();
@@ -30,11 +31,14 @@ router.post('/login', function(req, res, next){
     req.session.errors = errors;
     req.session.success = false;
   }else{
+    /*
     if(valid == true){
       req.session.success = true;
-    }
+    }*/
 
-    console.log("email is: " + req.body.address);
+
+
+    console.log("input account is: " + req.body.address);
   }
   // following action: this will call 1.
   res.redirect('/');
